c6d437b641936378ff45bbc176f67a5436dc9d93
Nirbhay Pundir <43448844+nirbhay-pundir@users.noreply.github.com>
2023-12-07 19:36:33 +0530

    Update check_update.dart

--- lib/src/screens/check_update.dart
+++ lib/src/screens/check_update.dart
@@ -27,154 +27,155 @@ class CheckUpdate extends StatelessWidget {
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
     checkUpdateController.startAnimation(context);
     AutoSize().init(context, 390, 844, 0);
-    StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-    builder: (context, snapshot) {
-    if (snapshot.hasData) {
-    return Dashboard();
-    } else {
     return StreamBuilder<User?>(
     stream: FirebaseAuth.instance.authStateChanges(),
     builder: (context, snapshot) {
-    if (snapshot.hasData) {
-    return Dashboard();
-    } else {
+      if (snapshot.hasData) {
+        return Dashboard();
+      } else {
         return SafeArea(
           child: Scaffold(
             body: Stack(
               children: [
                 Obx(
-                  () => AnimatedPositioned(
-                    duration: const Duration(milliseconds: 800),
-                    curve: Curves.easeOutBack,
-                    left: checkUpdateController.animating.value
-                        ? AutoSize.getAutoWidth(-127)
-                        : AutoSize.getAutoWidth(-511.51),
-                    top: checkUpdateController.animating.value
-                        ? AutoSize.getAutoHeight(314)
-                        : AutoSize.getAutoHeight(608.18),
-                    child: Hero(
-                      tag: 'oval',
-                      child: Container(
-                        width: AutoSize.getAutoWidth(517.14),
-                        height: AutoSize.getAutoHeight(698.85),
-                        decoration: ShapeDecoration(
-                          color: colorScheme.primary,
-                          shape: const OvalBorder(),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Obx(
-                  () => Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      SizedBox(
-                        height: AutoSize.getAutoHeight(321),
-                      ),
-                      Hero(
-                        tag: 'CSImage',
-                        child: Image.asset(
-                          CSImagePaths.updateCheckCSImage,
-                          height: AutoSize.getAutoHeight(204),
-                          width: AutoSize.getAutoWidth(202),
-                          fit: BoxFit.contain,
-                        ),
-                      ),
-                      SizedBox(
-                        height: AutoSize.getAutoHeight(100),
-                      ),
-                      if (checkUpdateController.updated.value)
-                        SizedBox(
-                          height: AutoSize.getAutoHeight(45),
-                        )
-                      else if (checkUpdateController.animated.value)
-                        Center(
-                          child: SizedBox(
-                            width: AutoSize.getAutoHeight(45),
-                            height: AutoSize.getAutoHeight(45),
-                            child: CircularProgressIndicator(
-                              strokeWidth: AutoSize.getAutoHeight(4),
-                              color: colorScheme.secondary,
+                      () =>
+                      AnimatedPositioned(
+                        duration: const Duration(milliseconds: 800),
+                        curve: Curves.easeOutBack,
+                        left: checkUpdateController.animating.value
+                            ? AutoSize.getAutoWidth(-127)
+                            : AutoSize.getAutoWidth(-511.51),
+                        top: checkUpdateController.animating.value
+                            ? AutoSize.getAutoHeight(314)
+                            : AutoSize.getAutoHeight(608.18),
+                        child: Hero(
+                          tag: 'oval',
+                          child: Container(
+                            width: AutoSize.getAutoWidth(517.14),
+                            height: AutoSize.getAutoHeight(698.85),
+                            decoration: ShapeDecoration(
+                              color: colorScheme.primary,
+                              shape: const OvalBorder(),
                             ),
                           ),
-                        )
-                      else
-                        SizedBox(
-                          height: AutoSize.getAutoHeight(45),
                         ),
-                      SizedBox(
-                        height: AutoSize.getAutoHeight(20),
                       ),
-                      AnimatedCrossFade(
-                        duration: const Duration(milliseconds: 500),
-                        crossFadeState: checkUpdateController.updated.value
-                            ? CrossFadeState.showFirst
-                            : CrossFadeState.showSecond,
-                        firstChild: ConstrainedBox(
-                          constraints: BoxConstraints(
-                            minHeight: AutoSize.getAutoHeight(30),
-                            maxHeight: AutoSize.getAutoHeight(30),
+                ),
+                Obx(
+                      () =>
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          SizedBox(
+                            height: AutoSize.getAutoHeight(321),
                           ),
-                          child: CsRegisterSignUpText(
+                          Hero(
+                            tag: 'CSImage',
+                            child: Image.asset(
+                              CSImagePaths.updateCheckCSImage,
+                              height: AutoSize.getAutoHeight(204),
+                              width: AutoSize.getAutoWidth(202),
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                          SizedBox(
+                            height: AutoSize.getAutoHeight(100),
+                          ),
+                          if (checkUpdateController.updated.value)
+                            SizedBox(
+                              height: AutoSize.getAutoHeight(45),
+                            )
+                          else
+                            if (checkUpdateController.animated.value)
+                              Center(
+                                child: SizedBox(
+                                  width: AutoSize.getAutoHeight(45),
+                                  height: AutoSize.getAutoHeight(45),
+                                  child: CircularProgressIndicator(
+                                    strokeWidth: AutoSize.getAutoHeight(4),
+                                    color: colorScheme.secondary,
+                                  ),
+                                ),
+                              )
+                            else
+                              SizedBox(
+                                height: AutoSize.getAutoHeight(45),
+                              ),
+                          SizedBox(
+                            height: AutoSize.getAutoHeight(20),
+                          ),
+                          AnimatedCrossFade(
+                            duration: const Duration(milliseconds: 500),
+                            crossFadeState: checkUpdateController.updated.value
+                                ? CrossFadeState.showFirst
+                                : CrossFadeState.showSecond,
+                            firstChild: ConstrainedBox(
+                              constraints: BoxConstraints(
+                                minHeight: AutoSize.getAutoHeight(30),
+                                maxHeight: AutoSize.getAutoHeight(30),
+                              ),
+                              child: CsRegisterSignUpText(
+                                colorScheme: colorScheme,
+                                color: colorScheme.background,
+                                onTap: () =>
+                                {
+                                  Navigator.pushNamed(
+                                      context, SignUpPersonal.route)
+                                },
+                                text1: CSStrings.newUser0,
+                                text2: CSStrings.newUser1,
+                              ),
+                            ),
+                            secondChild: Center(
+                              child: SizedBox(
+                                height: AutoSize.getAutoHeight(30),
+                                child: AutoSizeText(
+                                  minFontSize: 2,
+                                  CSStrings.checkUpdate,
+                                  textAlign: TextAlign.center,
+                                  style: CSTextStyles.bottomLine,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: AutoSize.getAutoHeight(40),
+                          ),
+                        ],
+                      ),
+                ),
+                Obx(
+                      () =>
+                      AnimatedPositioned(
+                        duration: const Duration(milliseconds: 800),
+                        curve: Curves.easeOutBack,
+                        left: checkUpdateController.updated.value
+                            ? AutoSize.getAutoWidth(81)
+                            : AutoSize.getAutoWidth(-511.51),
+                        top: AutoSize.getAutoHeight(625),
+                        child: Hero(
+                          tag: "Button",
+                          child: CsButton(
                             colorScheme: colorScheme,
-                            color: colorScheme.background,
-                            onTap: () => {
-                              Navigator.pushNamed(context, SignUpPersonal.route)
+                            text: CSStrings.signIn,
+                            backgroundColor: colorScheme.background,
+                            foregroundColor: colorScheme.primary,
+                            onClick: () =>
+                            {
+                              Navigator.pushNamed(
+                                context,
+                                SignIn.route,
+                              )
                             },
-                            text1: CSStrings.newUser0,
-                            text2: CSStrings.newUser1,
-                          ),
-                        ),
-                        secondChild: Center(
-                          child: SizedBox(
-                            height: AutoSize.getAutoHeight(30),
-                            child: AutoSizeText(
-                              minFontSize: 2,
-                              CSStrings.checkUpdate,
-                              textAlign: TextAlign.center,
-                              style: CSTextStyles.bottomLine,
-                            ),
                           ),
                         ),
                       ),
-                      SizedBox(
-                        height: AutoSize.getAutoHeight(40),
-                      ),
-                    ],
-                  ),
-                ),
-                Obx(
-                  () => AnimatedPositioned(
-                    duration: const Duration(milliseconds: 800),
-                    curve: Curves.easeOutBack,
-                    left: checkUpdateController.updated.value
-                        ? AutoSize.getAutoWidth(81)
-                        : AutoSize.getAutoWidth(-511.51),
-                    top: AutoSize.getAutoHeight(625),
-                    child: Hero(
-                      tag: "Button",
-                      child: CsButton(
-                        colorScheme: colorScheme,
-                        text: CSStrings.signIn,
-                        backgroundColor: colorScheme.background,
-                        foregroundColor: colorScheme.primary,
-                        onClick: () => {
-                          Navigator.pushNamed(
-                            context,
-                            SignIn.route,
-                          )
-                        },
-                      ),
-                    ),
-                  ),
                 ),
               ],
             ),
           ),
         );
       }
-    );
+    });
   }
 }
